6c745457d3f3c7e18b600b351ba9909db7fe6670
Rob Mayoff <git@rob.dqd.com>
2010-04-17 13:33:00 -0500

    refactor token/node objects; add support for prefix operators

--- dev/calc/parser.js
+++ dev/calc/parser.js
@@ -9,7 +9,10 @@ function parse(input) {
 	function beget(base, properties) {
 		function constructor() {}
 		constructor.prototype = base;
-		var object = new constructor(), key;
+		return extend(new constructor(), properties);
+	}
+	
+	function extend(object, properties) {
 		for (key in properties) {
 			object[key] = properties[key];
 		}
@@ -29,68 +32,106 @@ function parse(input) {
 		return consumed;
 	};
 	
-	var primitiveNode = {
-		parseAsPrefix: function() { throw 'Cannot use this kind of token as a prefix/standalone.'; },
-		parseAsSuffix: function() { throw 'Cannot use this kind of token as a suffix.'; },
+	var tokenTypes = {};
+
+	var primitiveType = {
+		parseAsPrefix: function() { throw 'Cannot use ' + this.name + ' as a prefix/standalone.'; },
+		parseAsSuffix: function() { throw 'Cannot use ' + this.name + ' as a suffix.'; },
 	};
 	
-	function makeEnd() {
-		return beget(primitiveNode, {
-			toString: function() { return '(end)'; },
-			precedence: 0,
+	function nodeToString(node) {
+		if (node === null) {
+			return 'null';
+		} else if (node === undefined) {
+			return 'undefined';
+		} else {
+			return node.toString();
+		}
+	}
+	
+	function simpleType(name, precedence, extensions) {
+		precedence = precedence || 0;
+		var type = tokenTypes[name];
+		if (type) {
+			type.precedence = Math.max(type.precedence, precedence);
+		} else {
+			type = tokenTypes[name] = beget(primitiveType, {
+				name: name,
+				precedence: precedence,
+				toString: function() { return this.name; },
+			});
+		}
+		extend(type, extensions);
+		return type;
+	}
+	
+	function infixType(name, precedence, extensions) {
+		var type = simpleType(name, precedence, {
+			parseAsSuffix: function(lhs) {
+				return extend(this, {
+					lhs: lhs,
+					rhs: expression(this.isRightAssociative ? this.precedence - 1 : this.precedence),
+				});
+			},
+			toString: function() {
+				return '(' + nodeToString(this.lhs) + ' ' + this.name + ' ' + nodeToString(this.rhs) + ')';
+			},
+		});
+		return extend(type, extensions);
+	}
+	
+	function prefixType(name, precedence, extensions) {
+		var type = simpleType(name, undefined, {
+			parseAsPrefix: function() {
+				this.rhs = expression(precedence);
+				return this;
+			},
+			toString: function() {
+				return '(' + this.name + ' ' + nodeToString(this.rhs) + ')';
+			}
 		});
 	}
 
-	function makeNumber(textLength) {
-		var text = Number(input.substr(offset, textLength));
-		offset += textLength;
-		return beget(primitiveNode, {
-			toString: function() { return text; },
-			parseAsPrefix: function() { return this; },
-			precedence: 0,
-		});
-	}
+	END = '(end)';
+	simpleType(END);
 	
-	function makeInfix(precedence) {
-		var operator = input[offset];
-		++offset;
-		return beget(primitiveNode, {
-			toString: function() { return [ '(', this.lhs.toString(), ' ', this.operator, ' ', this.rhs.toString(), ')' ].join(''); },
-			operator: operator,
-			precedence: precedence,
-			parseAsSuffix: function(lhs) {
-				this.lhs = lhs;
-				this.rhs = expression(this.precedence);
-				return this;
-			},
-		});
-	}
+	NUMBER = '(number)';
+	simpleType(NUMBER, undefined, {
+		toString: function() { return String(this.value); },
+		parseAsPrefix: function() { return this; },
+	});
+
+
+	infixType('+', 50);
+	infixType('-', 50);
+	infixType('*', 60);
+	infixType('/', 60);
+	prefixType('-', 80);
+	infixType('^', 90, { isRightAssociative: true, });
 
 	numberRe = /[0-9]+(?:\.[0-9]*)?|\.[0-9]+/g;
 
 	function lex() {
 		var c;
-		while (offset < length) {
+		while (true) {
+			if (offset >= length)
+				return beget(tokenTypes[END]);
 			c = input[offset];
 			if (c !== ' ')
 				break;
 			++offset;
 		}
-		if (offset >= length) {
-			return makeEnd();
-		}
-
-		switch (c) {
-			case '+': return makeInfix(50);
-			case '-': return makeInfix(50);
-			case '*': return makeInfix(60);
-			case '/': return makeInfix(60);
-			case '^': return makeInfix(80);
+		
+		if (c in tokenTypes) {
+			++offset;
+			return beget(tokenTypes[c]);
 		}
 
 		if ('0123456789.'.indexOf(c) >= 0) {
 			numberRe.lastIndex = offset;
-			return makeNumber(numberRe.exec(input)[0].length);
+			number = Number(numberRe.exec(input)[0]);
+			offset = numberRe.lastIndex;
+			return beget(tokenTypes[NUMBER], { value: number });
 		}
 
 		throw 'Invalid character "' + c + '" at offset ' + offset;
@@ -114,4 +155,4 @@ function parse(input) {
 	return expression(0);
 }
 
-console.log(parse('123 + 4.56^.78  ').toString());
+console.log(parse('--123 + -4.56^-.78^2  ').toString());
